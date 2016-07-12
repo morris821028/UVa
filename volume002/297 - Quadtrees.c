c4ae1ee39eae3f92766f105c4caf036523c31a86
Da Chen <chendamailbox@sina.com>
2016-07-12 17:23:14 +0800

    Update 297 - Quadtrees.c

--- volume002/297 - Quadtrees.c
+++ volume002/297 - Quadtrees.c
@@ -1,44 +1,46 @@
-#include <stdio.h>
-#include <string.h>
-int idx;
-char map[33][33];
-char tree[3024];
-void color(int x, int y, int b2) {
-    char c = tree[idx];
-     if(c == 'p') {
-        idx++;
-        color(x, y+b2/2, b2/2);
-        color(x, y, b2/2);
-        color(x+b2/2, y, b2/2);
-        color(x+b2/2, y+b2/2, b2/2);
-    } else if(c == 'f') {
-        int i, j;
-        for(i = x; i < x+b2; i++) {
-            for(j = y; j < y+b2; j++) {
-                map[i][j] = 1;
-            }
-        }
-        idx++;
-    } else
-        idx++;
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+const int len = 32;
+int mat[len][len];
+int cnt;
+char quad;
+
+void build(char q,int c,int r, int w)
+{
+	cin >> q;
+	if (q == 'p')
+	{
+		build(q, c+w/2, r,     w/2);
+		build(q, c,     r,     w/2);
+		build(q, c,     r+w/2, w/2);
+		build(q, c+w/2, r+w/2, w/2);
+	}
+	else if (q == 'f')
+		for (int i = r; i < (r + w);++i)
+			for (int j = c; j < (c + w); ++j)
+				if (mat[i][j] == 0)
+				{
+					mat[i][j] = 1;
+					cnt++;
+				}
 }
-int main() {
-    int t, i, j;
-    scanf("%d", &t);
-    getchar();
-    while(t--) {
-        memset(map, 0, sizeof(map));
-        scanf("%s", tree);
-        idx = 0;
-        color(0, 0, 32);
-        scanf("%s", tree);
-        idx = 0;
-        color(0, 0, 32);
-        int ans = 0;
-        for(i = 0; i < 32; i++)
-            for(j = 0; j < 32; j++)
-                ans += map[i][j];
-        printf("There are %d black pixels.\n", ans);
-    }
-    return 0;
+
+int main()
+{
+	int cas;
+	scanf("%d",&cas);
+	while (cas--)
+	{
+		cnt = 0;
+		memset(mat, 0, sizeof(mat));
+		build(quad, 0, 0, 32);
+		build(quad, 0, 0, 32);
+		cout << "There are " << cnt << " black pixels." << '\n';
+	}
+	system("pause");
+	return 0;
 }
