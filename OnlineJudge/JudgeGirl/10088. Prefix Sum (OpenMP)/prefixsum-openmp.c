b1441ef4cd30dd7b898478f0c68a96c29abcd2e5
Morris Yang <morris821028@gmail.com>
2017-02-09 21:36:23 +0800

    Parallel Prefix Sum, compiler optimization: pointer

--- OnlineJudge/JudgeGirl/10088. Prefix Sum (OpenMP)/prefixsum-openmp.c
+++ OnlineJudge/JudgeGirl/10088. Prefix Sum (OpenMP)/prefixsum-openmp.c
@@ -6,22 +6,22 @@
 #include "utils.h"
  
 #define MAXN 10000005
-uint32_t prefix_sum[MAXN], block[128] = {};
+uint32_t prefix_sum[MAXN], block[4] = {};
 int main() {
 	omp_set_num_threads(4);
     int n;
     uint32_t key;
     while (scanf("%d %" PRIu32, &n, &key) == 2) {
 		int MM = 4;
-		int chunk = 1;
 		#pragma omp parallel for
 		for (int i = 0; i < MM; i++) {
 			int l = i * n / MM + 1;
 			int r = (i+1) * n / MM;
 			uint32_t sum = 0;
+			uint32_t *ptr = prefix_sum + l;
 			for (int j = l; j <= r; j++) {
 				sum += encrypt(j, key);
-				prefix_sum[j] = sum;
+				*ptr = sum, ptr++;
 			}
 			block[i+1] = sum;
 		}
@@ -34,8 +34,9 @@ int main() {
 			int l = i * n / MM + 1;
 			int r = (i+1) * n / MM;
 			uint32_t sum = block[i];
+			uint32_t *ptr = prefix_sum + l;
 			for (int j = l; j <= r; j++) {
-				prefix_sum[j] += sum;
+				*ptr += sum, ptr++;
 			}
 		}
 		
@@ -43,3 +44,4 @@ int main() {
 	}
 	return 0;
 }
+
